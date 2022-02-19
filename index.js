2440b9f2b2f60b311436d474958e09186f24d776
KimYuRa1 <ura3118@naver.com>
2022-02-19 17:50:24 +0900

    220219_git생성

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ mongoose.connect('mongodb+srv://ura3118:ura62283118@my-app-cluster0.ahjly.mongod
     useNewUrlParser: true, useUnifiedTopology: true
 }).then( () => console.log('MongoDB connectted ...') ) //터미널에 뜨면 성공!
  .catch( err => console.log(err) ) // 에러코드 뜨면 연결 오류
+const {User2} = require("./models/User2");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+
+app.post('/register', (req,res) => {
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 DB에 넣어줌
+
+  //req.body 안에는 {name:“hello”, email:“~~”,password : “~”, ...} 등이 들어있는 것. =>bodyparser이 있어서 가능! 
+  const user = new User2(req.body);
+
+  //save: mongoDB의 메서드. 이 정보들이 User2모델에 저장됨
+  user.save( (err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
